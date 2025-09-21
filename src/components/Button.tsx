b4ebe14ca166d7603de13471aa46712df0eef2a7
Marouane Addou <105064179+marouaneaddou@users.noreply.github.com>
2025-09-21 14:05:00 +0100

    Update project page

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,7 +10,7 @@ const Button = ( props : buttonProp ) => {
 
     return <>
         <button onClick={ () => props.filterProject(props.category) } 
-            className={`flex gap-2 border py-2 px-4 rounded border-white/20 backdrop-blur-[10px]  
+            className={`w-fit flex gap-2  border py-2 px-4 rounded border-white/20 backdrop-blur-[10px]  
                 ${props.isActive ? "bg-orange-yellow text-black" : "bg-white/5 text-white/70"} `}>
             {getIcon(props.category)} {props.category}
         </button>
--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -38,7 +38,8 @@ const ProjectPage = () => {
                         <h1 className="flex font-bold mb-4 text-3xl gap-3"> <Code2 className="w-8 h-8"/> Projects</h1>
                         <p className="text-white/70 text-lg">A showcase of my development projects and technical achievements</p>
                 </div>
-                <div className="flex bg-white/5 p-6 rounded-lg justify-between max-w-3xl mx-auto backdrop-blur-[10px]  border border-white/10 text-white/50 mb-6">
+                <div className="flex flex-wrap gap-x-20 gap-y-4 w-fit
+                     bg-white/5 p-6 rounded-lg  max-w-3xl mx-auto backdrop-blur-[10px]  border border-white/10 text-white/50 mb-6">
                     <Button key={0} category={"All"} filterProject={filterProject} isActive={isActive=="All"}/>
                     { menuCategories.map( (val, idx) => (
                         <Button key={idx + 1} category={val} filterProject={filterProject} isActive={isActive==val}/>
